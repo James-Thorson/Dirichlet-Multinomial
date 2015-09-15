324d6a8957d476221af18d1aecb83b3aee64d1cc
kellijohnson <kellifayejohnson@gmail.com>
2015-09-15 11:32:07 -0700

    Use control.ss_new instead of hake_330.ctl for tune

--- R/tune_ctl.R
+++ R/tune_ctl.R
@@ -1,8 +1,8 @@
 #' Find a specific line in a \code{control.ss_new} file and then replace
-#' the numeric values in the \code{.ctl} file at that specific 
+#' the numeric values in the \code{.ctl} file at that specific
 #' line with the information provided in \code{replace}.
 #' A new file will be written to \code{hake_330.ctl} file in the same
-#' directory that the \code{control.ss_new} file was found. 
+#' directory that the \code{control.ss_new} file was found.
 #' If \code{is.null(folder)} is \code{TRUE} then \code{folder} will be set
 #' to the current \code{getwd()}.
 
@@ -10,7 +10,7 @@ tune_ctl <- function(word = "#_mult_by_agecomp_N", folder = NULL, replace = new)
   # get directory and filename
   if(is.null(folder)) folder <- getwd()
   folder <- gsub("/$", "", folder)
-  ctlname <- list.files(folder, pattern = "ctl", full.names = TRUE)
+  ctlname <- list.files(folder, pattern = "control", full.names = TRUE)
   # read the ctl
   ctl <- readLines(ctlname)
 
@@ -33,7 +33,7 @@ tune_ctl <- function(word = "#_mult_by_agecomp_N", folder = NULL, replace = new)
   } else {
   	newword <- paste(paste(replace, collapse = " "), word)
   }
-  
+
   ctl[line[keep]] <- newword
   writeLines(ctl, file.path(folder, "hake_330.ctl"))
 
