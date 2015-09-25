68432e75b6b3988d757603ff219dc4fd6fc733e2
James-Thorson <JamesT.esq@gmail.com>
2015-09-25 12:25:34 -0700

    small changes to figure plotting

--- Plot.R
+++ Plot.R
@@ -11,8 +11,9 @@ colors <- gray.colors(3, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL)
 ###############################################################################
 png(filename = paste0(ResultsFD, "/DM1_inflation.png"), res = resolution,
   width = width, height = height)
+par( mar=c(3,3,0.5,0.5), mgp=c(1.75,0.5,0), tck=-0.02, oma=c(0,0,0,0))
 boxplot(1/exp(lnEffN_mult_1) ~ Nfishery + ntrue,
-  data = droplevels(subset(resdf, Nfishery != 1)), las = 1,
+  data = droplevels(subset(resdf, Nfishery != 1)),
   xlab = "OM yearly sample size", ylab = expression(theta),
   col = colors, xaxt = "n")
 axis(1, at = c(2, 5, 8), labels = yearlyn)
@@ -27,8 +28,9 @@ resdf$ESS1 <- with(resdf, ((1/nsamp) + (1/(exp(lnEffN_mult_1) + 1)))^(-1))
 resdf$ESS2 <- with(resdf, ((1/nsamp) + (1/(exp(lnEffN_mult_1) * as.numeric(as.character(nsamp)) + 1)))^(-1))
 png(filename = paste0(ResultsFD, "/ESS_inflation.png"), res = resolution,
     width = width, height = height)
+par( mar=c(3,3,0.5,0.5), mgp=c(1.75,0.5,0), tck=-0.02, oma=c(0,0,0,0))
 boxplot(ESS2 ~ Nfishery + ntrue,
-        data = droplevels(subset(resdf, Nfishery != 1)), las = 1,
+        data = droplevels(subset(resdf, Nfishery != 1)),
   xlab = "OM yearly sample size", ylab = expression(N[eff]),
   col = colors, xaxt = "n")
 axis(1, at = c(2, 5, 8), labels = yearlyn)
@@ -39,9 +41,11 @@ dev.off()
 ###############################################################################
 # Correlated error in R_0 and M
 ###############################################################################
+library(ggplot2)
 gradientfilter <- 0.1
 png(filename = paste0(ResultsFD, "/R0andM_inflation.png"), res = resolution,
     width = width, height = height)
+par( mar=c(3,3,0.5,0.5), mgp=c(1.75,0.5,0), tck=-0.02, oma=c(0,0,0,0))
 means <- aggregate(gradient ~ model + Nfishery, data = resdf, mean)
 means$gradient <- round(means$gradient, 4)
 lengths <- aggregate(gradient ~ model + Nfishery,
