a7d4a255f25e109e5329d96b5f41db6ceac53b38
Jim Thorson <JamesT.esq@gmail.com>
2016-02-28 18:04:45 -0800

    update hake case study and Pearson resid plot

---
+++ hake_case_study_results/Compare_results_2016-02-28.R
@@ -0,0 +1,51 @@
+
+if(!"ThorsonUtilities" %in% installed.packages()[, 1]) {
+  devtools::install_github("james-thorson/utilities")
+}
+library(ThorsonUtilities)
+library( r4ss )
+
+RootFile = "C:/Users/James.Thorson/Desktop/Project_git/Dirichlet-Multinomial/hake_case_study_results/hake_V3.3_explicitF_V4/"
+  #SsOutput = Covar = TRUE
+  #SsOutput = SS_output(RunFile, covar=Covar, forecast=TRUE, printstats=TRUE, ncols=210)
+  # All
+  #Plots = SS_plots(SsOutput, png=TRUE, printfolder="Plots_Default", uncertainty=Covar, aalresids=TRUE, datplot=TRUE, areanames=c("CA","OR","WA"), multifig_colpolygon=c( rgb(1,0,0,0.5), rgb(0,0,1,0.5), rgb(0,1,0,0.5)), multifig_oma=c(5,5,1,2)+.1, linescol=c("green", "red", "blue"), andre_oma=c(3,0,0.5,0) ) #aalyear, aalbin
+
+RunFile = paste0(RootFile,"without fishery/")
+  #STD_3 = read.table( paste0(RunFile,"ss3.std"), header=TRUE )
+  Output = SS_output(RunFile, covar=TRUE, forecast=TRUE, printstats=TRUE)
+  STD_0 = Output$derived_quants
+RunFile = paste0(RootFile,"without D-M unweighted/")
+  #STD_0 = read.table( paste0(RunFile,"ss3.std"), header=TRUE )
+  Output = SS_output(RunFile, covar=TRUE, forecast=TRUE, printstats=TRUE)
+  STD_1 = Output$derived_quants
+RunFile = paste0(RootFile,"without D-M/")
+  #STD_1 = read.table( paste0(RunFile,"ss3.std"), header=TRUE )
+  Output = SS_output(RunFile, covar=TRUE, forecast=TRUE, printstats=TRUE)
+  STD_2 = Output$derived_quants
+RunFile = paste0(RootFile,"with D-M/")
+  #STD_2 = read.table( paste0(RunFile,"ss3.std"), header=TRUE )
+  Output = SS_output(RunFile, covar=TRUE, forecast=TRUE, printstats=TRUE)
+  STD_3 = Output$derived_quants
+
+YearSet = 1967:2013
+save_fig( file=paste0(RootFile,"Comparison_with_hake"), width=6.5, height=6.5, res=c(200,600), type=c("png","tif"), FUN=function(){
+  par( mfrow=c(2,2), mar=c(0,2,2,0), mgp=c(1.75,0.25,0), xaxs="i", yaxs="i", oma=c(3,1,0,0))
+  for(j in 1:4){
+    #Name = c("depletion", "SPB_std", "F_std")[j]
+    Name = paste0( c("Bratio_", "SPB_", "F_", "Recr_")[j], YearSet)
+    Label = c("Relative spawning output", "Spawning output", "Fishing intensity", "Recruitment")[j]
+    Est = cbind( STD_0[Name,c('Value','StdDev')], STD_1[Name,c('Value','StdDev')], STD_2[Name,c('Value','StdDev')], STD_3[Name,c('Value','StdDev')] )
+    Ylim = c(0,1.3*max(Est[,c(1,3,5,7)]))
+    if( j==3 ) Ylim[2] = Ylim[2] * 1.3
+    matplot( y=Est[,c(1,3,5,7)], x=YearSet, type="l", col=c("red","green","black","blue"), lty="solid", lwd=3, ylim=Ylim, xlab="", main=Label, ylab="", xaxt="n", yaxt="n" )
+    for(i in 1:4){
+      polygon( y=c(Est[,1+2*(i-1)]-1.0*Est[,2+2*(i-1)],rev(Est[,1+2*(i-1)]+1.0*Est[,2+2*(i-1)])), x=c(YearSet,rev(YearSet)), col=list(rgb(1,0,0,0.2),rgb(0,1,0,0.2),rgb(0,0,0,0.2),rgb(0,0,1,0.2))[[i]], border=NA )
+    }
+    if(j==3) legend("topleft", legend=c("No fishery ages","Unweighted","Tuned","D-M"), fill=c("red","green","black","blue"), ncol=2, bty="n")
+    if(j>=3) axis(1)
+    axis(2, at=rev(rev(axTicks(2))[-1]), labels=rev(rev(axTicks(2))[-1]) )
+  }
+  mtext(side=1, text="Year", outer=TRUE, line=2)
+  mtext(side=2, text="Derived quantity", outer=TRUE, line=0)
+})
--- hake_case_study_results/Pearon_figure_Dirichlet-Multinomial.R
+++ hake_case_study_results/Pearon_figure_Dirichlet-Multinomial.R
@@ -1,19 +1,28 @@
 
+if(!"ThorsonUtilities" %in% installed.packages()[, 1]) {
+  devtools::install_github("james-thorson/utilities")
+}
+library(ThorsonUtilities)
 library( r4ss )
 # Pearson residuals for Jim Thorson's request on 2/25/16
 #dir <- 'C:/github/Dirichlet-Multinomial'
 dir <- 'C:/Users/James.Thorson/Desktop/Project_git/Dirichlet-Multinomial/hake_case_study_results/'
-if(!exists('dm4')) dm4 <- SS_output(file.path(dir, 'hake_V3.3_explicitF_V4/with D-M/hake_V3.3_explicitF_V4'))
+dm4 <- SS_output(file.path(dir, 'hake_V3.3_explicitF_V4/with D-M/'))
 source(file.path(dir,'SSplotComps_Dirichlet-Multinomial.R'))
+
+# Log-param
+DM_param = exp( dm4$parameter['ln(EffN_mult)_1','Value'] )
+# Var_adjust = (1 + DM_param*N_input) / (1 + DM_param)
+# Line 1526 from SSplotComps
+dm4$agedbase[,'Pearson'] = dm4$agedbase[,'Pearson'] / sqrt((1 + DM_param*dm4$agedbase[,'N']) / (1 + DM_param))
 
 ## Pearson residuals
 doPNG <- T
 ht <- 7; wd<- 6
-if(doPNG) {png(file.path(dir,"ageCompPearsons.png"),height=ht,width=wd,
-               pointsize=10,units="in",res=300)}
-if(!doPNG) {windows(width=wd,height=ht)}
-SSplotComps(dm4,kind="AGE",subplot=24,printmkt=FALSE,printsex=FALSE,
-            fleetnames=c("Fishery","Survey"), axis1=seq(1975,2015,5))
-mtext("Year",side=1,line=3,cex=par()$cex)
-if(doPNG){dev.off()}
+save_fig( file=paste0(RootFile,"Pearson_Residuals"), width=6.5, height=6.5, res=c(200,600), type=c("png","tif"), FUN=function(){
+  #par( oma=c(4,4,0,0) )
+  SSplotComps(dm4,kind="AGE",subplot=24,printmkt=FALSE,printsex=FALSE, fleetnames=c("Fishery","Survey"), axis1=seq(1975,2015,5))
+  mtext("Year",side=1,line=3,cex=par()$cex,outer=TRUE)
+  #mtext("Age",side=1,line=3,cex=par()$cex,outer=TRUE)
+})
 
