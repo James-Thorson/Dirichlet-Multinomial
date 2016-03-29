b6b7c81f42e0e928030af232fefb3feae3d40265
Jim Thorson <JamesT.esq@gmail.com>
2016-03-28 18:09:43 -0700

    added toy example of DM parameterization...
    ... where I can play with emphasis factors and visualize the impact on
    DM parameter estimates

--- hake_case_study_results/Pearon_figure_Dirichlet-Multinomial.R
+++ hake_case_study_results/Pearon_figure_Dirichlet-Multinomial.R
@@ -13,22 +13,26 @@ dir <- 'C:/Users/James.Thorson/Desktop/Project_git/Dirichlet-Multinomial/hake_ca
 source(file.path(dir,'SSplotComps_Dirichlet-Multinomial.R'))
 
 # Pearson residuals for Jim Thorson's request on 2/25/16
-for(i in 1:3){
+for(i in 1:4){
   if(i==1) model_dir <- file.path(dir,'hake_V3.3_explicitF_V4/with D-M/')
   if(i==2) model_dir <- file.path(dir,'hake_V3.3_explicitF_V4/without D-M/')
   if(i==3) model_dir <- file.path(dir,'hake_V3.3_explicitF_V4/without D-M unweighted/')
+  if(i==4) model_dir <- file.path(dir,'hake_V3.3_explicitF_V4/with D-M (ad hoc lambdas)/')
   dm4 <- SS_output(model_dir)
 
   # Log-param
-  if(i==1){
+  if(i==1 | i==4){
     DM_param = exp( dm4$parameter['ln(EffN_mult)_1','Value'] )
     # Var_adjust = (1 + DM_param*N_input) / (1 + DM_param)
     # Line 1526 from SSplotComps
 
     # Linear
+    #dm4$agedbase[,'effective_sample_size'] = (1 + DM_param * dm4$agedbase[,'N']) / (1 + DM_param)
     dm4$agedbase[,'effective_sample_size'] = (1 + DM_param * dm4$agedbase[,'N']) / (1 + DM_param)
     # Saturating
     #dm4$agedbase[,'effective_sample_size'] = (dm4$agedbase[,'N'] + DM_param*dm4$agedbase[,'N']) / (dm4$agedbase[,'N'] + DM_param)
+    # Ratio
+    dm4$agedbase[,'sample_size_ratio'] = dm4$agedbase[,'effective_sample_size'] / dm4$agedbase[,'N']
 
     # Calculation
     dm4$agedbase[,'sd'] = sqrt( dm4$agedbase[,'Exp']*(1-dm4$agedbase[,'Exp']) / dm4$agedbase[,'effective_sample_size'] )
